2b651d101cc5d8966a983afea37c12a47a6b9e13
William Dias <william.jose.wjd@gmail.com>
2020-12-20 12:04:04 -0300

    finalizando desafio

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acm, tot) => {
+      const productsTotal = tot.price * tot.quantity;
+      return acm + productsTotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acm, tot) => {
+      const productsTotal = tot.quantity;
+      return acm + productsTotal;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
